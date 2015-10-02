e01b3555c0ae4a45bc6d0eee83d94883398a54aa
Vlad Ungureanu <Vlad.Ungureanu@Endava.com>
2015-10-02 18:41:43 +0300

    Swing examples

---
+++ projects/5 - swing/src/ro/learnstuff/main/SwingMain.java
@@ -0,0 +1,11 @@
+package ro.learnstuff.main;
+
+import ro.learnstuff.frame.ExampleFrame;
+
+public class SwingMain {
+
+	public static void main(String[] args) {
+		ExampleFrame exampleFrame = new ExampleFrame("Testing frame!");
+		exampleFrame.setVisible(true);
+	}
+}
